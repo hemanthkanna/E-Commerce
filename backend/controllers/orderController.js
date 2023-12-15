b90f43a3e4dc64529aa94d976bd849faf1deb555
Hemanthraj-Thiyagarajan <hemanthraj.thiyagarajan@gmail.com>
2023-12-15 14:54:34 +0530

    Get All Order , Update Order/Status API

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 const catchAsyncError = require("../middlewares/catchAsyncError");
 const Order = require("../models/orderModel");
+const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 
 //Create New order - api/v1/order/new
@@ -56,3 +57,49 @@ exports.myOrders = catchAsyncError(async (req,res,next) => {
         orders
     })
 })
+
+//Admin Get All Orders - api/v1/orders
+exports.orders = catchAsyncError(async (req,res,next) => {
+    const orders = await Order.find();
+
+    let totalAmount = 0;
+
+    orders.forEach(order => {
+        totalAmount += order.totalPrice;
+    })
+
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders
+    })
+})
+
+//Admin: Update Order / Order staus  -  api/v1/order/:id
+exports.updateOrder = catchAsyncError(async (req,res,next) => {
+    const order = await Order.findById(req.params.id);
+
+    if(order.orderStatus == 'Delivered') {
+        return next(new ErrorHandler('Order has beeen already delivered'));
+    }
+
+    //Updating the product stock of each order item
+    order.orderItems.forEach( async orderItem => {
+        await updateStock(orderItem.product, orderItem.quantity)
+    })
+
+    order.orderStatus = req.body.orderStatus;
+    order.deliveredAt = Date.now();
+    await order.save();
+
+    res.status(200).json({
+        success:true
+    })
+})
+
+async function updateStock(productId, quantity) {
+    const product = await Product.findById(productId);
+    product.stock = product.stock - quantity;
+    product.save({validateBeforeSave:false});
+}
+
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { newOrder, getSingleOrder, myOrders } = require('../controllers/orderController');
+const { newOrder, getSingleOrder, myOrders, orders, updateOrder } = require('../controllers/orderController');
 const router = express.Router();
-const { isAuthenticatedUser } = require('../middlewares/authenticate');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/authenticate');
 
 router.route('/order/new').post(isAuthenticatedUser,newOrder);
 router.route('/order/:id').get(isAuthenticatedUser,getSingleOrder);
 router.route('/myorders').get(isAuthenticatedUser,myOrders);
+
+//Admin Routes
+router.route('/orders').get(isAuthenticatedUser,authorizeRoles('admin'),orders);
+router.route('/order/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateOrder);
 
 module.exports = router;
