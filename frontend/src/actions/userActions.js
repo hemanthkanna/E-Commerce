ec741a19bb55b8fcb49c5f938aed8dbdaca965e8
Hemanthraj-Thiyagarajan <hemanthraj.thiyagarajan@gmail.com>
2024-01-03 15:15:06 +0530

    Admin Dashboard - userList , update , delete user

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -24,6 +24,22 @@ import {
   forgotPasswordRequest,
   updateProfileSuccess,
 } from "../slices/authSlice";
+
+import {
+  usersRequest,
+  usersSuccess,
+  usersFail,
+  userRequest,
+  userSuccess,
+  userFail,
+  deleteUserRequest,
+  deleteUserSuccess,
+  deleteUserFail,
+  updateUserRequest,
+  updateUserSuccess,
+  updateUserFail,
+} from "../slices/userSlice";
+
 import axios from "axios";
 
 export const login = (email, password) => async (dispatch) => {
@@ -94,49 +110,98 @@ export const updateProfile = (userData) => async (dispatch) => {
 
 export const updatePassword = (formData) => async (dispatch) => {
   try {
-    dispatch(updatePasswordRequest())
+    dispatch(updatePasswordRequest());
     const config = {
-        headers: {
-            'Content-type': 'application/json'
-        }
-    }
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
     await axios.put(`/api/v1/password/change`, formData, config);
-    dispatch(updatePasswordSuccess())
-} catch (error) {
-    dispatch(updatePasswordFail(error.response.data.message))
-}
+    dispatch(updatePasswordSuccess());
+  } catch (error) {
+    dispatch(updatePasswordFail(error.response.data.message));
+  }
 };
 
 export const forgotPassword = (formData) => async (dispatch) => {
-
   try {
-      dispatch(forgotPasswordRequest())
-      const config = {
-          headers: {
-              'Content-type': 'application/json'
-          }
-      }
-      const { data} =  await axios.post(`/api/v1/password/forgot`, formData, config);
-      dispatch(forgotPasswordSuccess(data))
+    dispatch(forgotPasswordRequest());
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      `/api/v1/password/forgot`,
+      formData,
+      config
+    );
+    dispatch(forgotPasswordSuccess(data));
   } catch (error) {
-      dispatch(forgotPasswordFail(error.response.data.message))
+    dispatch(forgotPasswordFail(error.response.data.message));
   }
-
-}
+};
 
 export const resetPassword = (formData, token) => async (dispatch) => {
-
   try {
-      dispatch(resetPasswordRequest())
-      const config = {
-          headers: {
-              'Content-type': 'application/json'
-          }
-      }
-      const { data} =  await axios.post(`/api/v1/password/reset/${token}`, formData, config);
-      dispatch(resetPasswordSuccess(data))
+    dispatch(resetPasswordRequest());
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      `/api/v1/password/reset/${token}`,
+      formData,
+      config
+    );
+    dispatch(resetPasswordSuccess(data));
   } catch (error) {
-      dispatch(resetPasswordFail(error.response.data.message))
+    dispatch(resetPasswordFail(error.response.data.message));
   }
+};
 
-}
+export const getUsers = async (dispatch) => {
+  try {
+    dispatch(usersRequest());
+    const { data } = await axios.get(`/api/v1/admin/users`);
+    dispatch(usersSuccess(data));
+  } catch (error) {
+    dispatch(usersFail(error.response.data.message));
+  }
+};
+
+export const getUser = (id) => async (dispatch) => {
+  try {
+    dispatch(userRequest());
+    const { data } = await axios.get(`/api/v1/admin/user/${id}`);
+    dispatch(userSuccess(data));
+  } catch (error) {
+    dispatch(userFail(error.response.data.message));
+  }
+};
+
+export const deleteUser = (id) => async (dispatch) => {
+  try {
+    dispatch(deleteUserRequest());
+    await axios.delete(`/api/v1/admin/user/${id}`);
+    dispatch(deleteUserSuccess());
+  } catch (error) {
+    dispatch(deleteUserFail(error.response.data.message));
+  }
+};
+
+export const updateUser = (id, formData) => async (dispatch) => {
+  try {
+    dispatch(updateUserRequest());
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+    await axios.put(`/api/v1/admin/user/${id}`, formData, config);
+    dispatch(updateUserSuccess());
+  } catch (error) {
+    dispatch(updateUserFail(error.response.data.message));
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,14 +5,15 @@ import ProductReducer from "./slices/productSlice";
 import authReducer from './slices/authSlice';
 import cartReducer from './slices/cartSlice';
 import orderReducer from './slices/orderSlice';
+import userReducer from './slices/userSlice';
 
 const reducer = combineReducers({
     productsState: ProductsReducer,
     productState: ProductReducer,
     authState: authReducer,
     cartState: cartReducer,
-    orderState: orderReducer
-
+    orderState: orderReducer,
+    userState: userReducer,
 });
 const store = configureStore({
   reducer,
